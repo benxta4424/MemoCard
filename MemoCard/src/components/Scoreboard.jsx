a2334433d52a432cfa5eeeba588bd77130028f0d
benxta4424 <podean.beni@gmail.com>
2025-09-13 18:19:58 +0300

    adding the final touches

--- MemoCard/src/components/Scoreboard.jsx
+++ MemoCard/src/components/Scoreboard.jsx
@@ -5,7 +5,11 @@ export default function Scoreboard({counter}) {
 
         <>
             <div className="scoreboardContainer">
-                
+                <div className="title">Have fun with MemoCard</div>
+                <div className="score"> 
+                    { counter < 12 && `${counter} / 12` }
+                    { counter == 12 && `Congrats! You've won.`  }
+                </div>
             </div>
         </>
 
